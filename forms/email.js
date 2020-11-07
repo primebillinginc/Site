b496c651c70b3b8b8904179e8e1ff1789a2d2ac6
primebillinginc <73267296+primebillinginc@users.noreply.github.com>
2020-11-07 08:59:19 -0500

    Add files via upload

--- forms/email.js
+++ forms/email.js
@@ -1,9 +1,4 @@
 		function sendEmail() {
-
-			// if (document.getElementById("name").value = ' ') {
-			// 	alert("Error")
-			// } else {
-			
 			Email.send({
 				Host: "smtp.gmail.com",
 				Username : "siteprimebillinginc@gmail.com",
@@ -21,22 +16,27 @@
 		}
 		
 		function subscribe() {
-		    if (document.getElementById("emailsubscribe").value != '') {
-			 
-			Email.send({
-				Host: "smtp.gmail.com",
-				Username : "siteprimebillinginc@gmail.com",
-				Password : "Mikipass.1980",
-				To : "siteprimebillinginc@gmail.com",
-				From : "siteprimebillinginc@gmail.com",
-				Subject : "Subscribe",
-				Body : document.getElementById("emailsubscribe").value + " need subscribe to www.primebillinginc.com",
-			})
-			.then(function(message){
-				//  alert("Successful subscription"),
-				document.getElementById("formsubscribe").reset();
-				document.getElementById("emailsubscribe").focus();
-			});
+			valor = document.getElementById("emailsubscribe").value;
+				alert("Successful");
+				if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(valor)){
+					Email.send({
+						Host: "smtp.gmail.com",
+						Username : "siteprimebillinginc@gmail.com",
+						Password : "Mikipass.1980",
+						To : "siteprimebillinginc@gmail.com",
+						From : "siteprimebillinginc@gmail.com",
+						Subject : "Subscribe",
+						Body : document.getElementById("emailsubscribe").value + " need subscribe to www.primebillinginc.com",
+					})
+					.then(function(message){
+						alert("Successful subscription"),
+						document.getElementById("formsubscribe").reset();
+						document.getElementById("emailsubscribe").focus();
+					});
+				} 
+				else {
+					   document.getElementById("emailsubscribe").focus(); 
+				}
 		}
-	}
+
 
