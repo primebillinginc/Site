233d05883dc812a49fd7246720d9d5a775d9420c
primebillinginc <73267296+primebillinginc@users.noreply.github.com>
2020-11-07 09:06:59 -0500

    Add files via upload

--- forms/email.js
+++ forms/email.js
@@ -17,7 +17,6 @@
 		
 		function subscribe() {
 			valor = document.getElementById("emailsubscribe").value;
-				alert("Successful");
 				if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(valor)){
 					Email.send({
 						Host: "smtp.gmail.com",
